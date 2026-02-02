cdfe8f2ccb6bb95f5781877dc76bc768f54bd8ea
kasraxrr <kasrafar1383@gmail.com>
2026-02-02 10:29:52 +0100

    yuguyfm

---
+++ Opgave1b/src/RunnableClock.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class RunnableClock implements Runnable{
+    private DateTimeFormatter timeFormatter;
+
+    public RunnableClock(){
+        timeFormatter=DateTimeFormatter.ofPattern("HH:mm:ss");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < -1; i++) {
+            LocalDate time=LocalDate.now();
+            System.out.println(time.format(timeFormatter));
+
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
---
+++ Opgave1b/src/View/TemperatureViewController.java
@@ -0,0 +1,14 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+
+import java.awt.*;
+
+public class TemperatureViewController {
+    @FXML private TextField temperature;
+    private Region root;
+    private LoginModel loginModel;
+    private ViewHandler viewHandler;
+
+}
