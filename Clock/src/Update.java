16ae26de2b5704000ad1dd66b87c877266575bba
kasraxrr <kasrafar1383@gmail.com>
2026-02-06 17:16:22 +0100

    yuguyfmkjb

--- Clock/src/Update.java
+++ Clock/src/Update.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Update implements Runnable{
     private Clock clock;
+
+    public Update(Clock clock){
+        this.clock=clock;
+    }
+    @Override
+    public void run(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the hour");
+        int hour=scanner.nextInt();
+        System.out.println("enter the minute");
+        int minute=scanner.nextInt();
+        System.out.println("enter the second");
+        int second=scanner.nextInt();
+        clock.set(hour,minute,second);
+    }
+
 
 }
---
+++ Clock/src/main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args) {
+        Clock clock=new Clock();
+
+        Show show=new Show(clock);
+        Update update=new Update(clock);
+
+        Thread t1=new Thread(show,"show");
+        Thread t2=new Thread(update,"update");
+
+        t1.start();
+        t2.start();
+
+
+
+
+        System.out.println("main ended");
+
+    }
+}
