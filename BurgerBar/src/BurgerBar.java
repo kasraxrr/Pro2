6b006ee5d525e7746548e0704abe74c32ac2b8a3
kasraxrr <kasrafar1383@gmail.com>
2026-02-07 18:40:55 +0100

    yuguyfmkjb

--- BurgerBar/src/BurgerBar.java
+++ BurgerBar/src/BurgerBar.java
@@ -1,3 +1,35 @@
 public class BurgerBar {
     private int maxNumberOfBurgers;
+    private int numberOfBurgers;
+
+    public BurgerBar(int maxNumberOfBurgers){
+        this.numberOfBurgers=0;
+        this.maxNumberOfBurgers=maxNumberOfBurgers;
+    }
+    public synchronized void makeBurger(String employeeName){
+        while (numberOfBurgers>=maxNumberOfBurgers){
+            try {
+                System.out.println("cant make any more now");
+                wait();
+            }catch (InterruptedException e){
+
+            }
+
+        }
+        numberOfBurgers++;
+        System.out.println("made a burger");
+    }
+    public synchronized void eatBurger(String who){
+        while (numberOfBurgers<=0){
+            try {
+                System.out.println("cant make any more now");
+                wait();
+            }catch (InterruptedException e){
+
+            }
+
+        }
+        numberOfBurgers--;
+        System.out.println("made a burger");
+    }
 }
