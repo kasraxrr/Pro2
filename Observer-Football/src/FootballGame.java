12c0640179d7713248879d15f43da60981b813b7
kasraxrr <kasrafar1383@gmail.com>
2026-02-16 10:55:59 +0100

    yuguyfmkjb

--- Observer-Football/src/FootballGame.java
+++ Observer-Football/src/FootballGame.java
@@ -1,2 +1,64 @@
-public class FootballGame {
+import utility.observer.javaobserver.NamedPropertyChangeSubject;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class FootballGame implements NamedPropertyChangeSubject
+{
+    private String homeTeam;
+    private String awayTeam;
+    private int homeTeamGoal;
+    private int awayTeamGoal;
+    private PropertyChangeSupport property;
+    public FootballGame(String homeTeam, String awayTeam)
+    {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamGoal = 0;
+        this.awayTeamGoal = 0;
+        this.property=new PropertyChangeSupport(this);
+    }
+    public String getHomeTeam()
+    {
+        return homeTeam;
+    }
+    public String getAwayTeam()
+    {
+        return awayTeam;
+    }
+    public void scoreGoal(String team)
+    {
+        if (team.equals(homeTeam))
+        {
+
+            homeTeamGoal++;
+
+        }
+        else if (team.equals(awayTeam))
+        {
+
+            awayTeamGoal++;
+        }
+        property.firePropertyChange("goal",team,getScore());
+    }
+    public String getScore()
+    {
+        return homeTeamGoal + " - " + awayTeamGoal;
+    }
+    public String endGame()
+    {
+        return getScore();
+    }
+
+    @Override
+    public void addListener(String propertyName,PropertyChangeListener listener) {
+    property.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removeListener(String propertyName,PropertyChangeListener listener) {
+    property.removePropertyChangeListener(listener);
+    }
+
+
 }
--- Observer-Football/src/Scoreboard.java
+++ Observer-Football/src/Scoreboard.java
@@ -1,2 +1,22 @@
-public class Scoreboard {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Scoreboard implements PropertyChangeListener {
+    private FootballGame game;
+    public Scoreboard(FootballGame game)
+    {
+        this.game = game;
+        showScore(game.getScore());
+        game.addListener(game.getHomeTeam(), this);
+        game.addListener(game.getAwayTeam(), this);
+    }
+    public void showScore(String score)
+    {
+        System.out.println("SCOREBOARD: " + score);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        showScore(evt.getNewValue().toString());
+    }
 }
