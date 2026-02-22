154efbdd8d9f0a633b4de678fd7dc2f879b65296
kasraxrr <kasrafar1383@gmail.com>
2026-02-22 11:51:02 +0100

    yuguyfmkjb

--- Observer-BusStop/src/Bus.java
+++ Observer-BusStop/src/Bus.java
@@ -1,14 +1,54 @@
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-
-public class Bus implements UnnamedPropertyChangeSubject{
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+public class Bus implements UnnamedPropertyChangeSubject
+{
     private String info;
-    private PropertyChangeSupport property;
     private Set<BusPassenger> passengers;
-
-    public Bus(){
+    private PropertyChangeSupport property;
+    public Bus()
+    {
+        passengers = new HashSet<BusPassenger>();
+        this.info = "eXpress " + hashCode() % 100;
         property=new PropertyChangeSupport(this);
     }
-    public void passengerGettingIn(BusPassenger p){
+    public void passengerGettingIn(BusPassenger p)
+    {
+        Iterator<BusPassenger> it = passengers.iterator();
+        while (it.hasNext())
+            it.next().showMessage("Enter:"+p.getName());
+        passengers.add(p);
+        property.firePropertyChange("Enter"+p.getName(),null,p);
+    }
+    public void passengerGettingOut(BusPassenger p)
+    {
+        passengers.remove(p);
+        Iterator<BusPassenger> it = passengers.iterator();
+        while (it.hasNext())
+            it.next().showMessage("Leave:"+p.getName());
+        property.firePropertyChange("Leave"+p.getName(),null,p);
+    }
+    public String toString()
+    {
+        return info;
+    }
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Bus))
+            return false;
+        Bus b = (Bus) obj;
+        return info.equals(b.info);
+    }
 
+    @Override
+    public void addListener(PropertyChangeListener listener) {
+        property.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removeListener(PropertyChangeListener listener) {
+    property.removePropertyChangeListener(listener);
     }
 }
--- Observer-BusStop/src/BusStop.java
+++ Observer-BusStop/src/BusStop.java
@@ -1,2 +1,40 @@
-public class BusStop {
+public class BusStop
+{
+    public static void main(String args[])
+            throws InterruptedException
+    {
+        Bus bus = new Bus();
+        System.out.println("Bus ID: " + bus);
+        System.out.println();
+        BusPassenger[] passengers = new BusPassenger[4];
+        passengers[0] = new BusPassenger("Allan");
+        passengers[1] = new BusPassenger("Britt");
+        passengers[2] = new BusPassenger("Carl");
+        passengers[3] = new BusPassenger("Dee");
+        for (int i = 0; i < passengers.length; i++)
+        {
+            System.out.println(passengers[i] + " is getting in");
+            passengers[i].getIn(bus);
+            Thread.sleep(6000);
+        }
+        System.out.println(passengers[2] + " is getting out");
+        passengers[2].getOut();
+    }
 }
+/* OUTPUT: (note Buss id could be different)
+Bus ID: eXpress 22
+Allan is getting in
+Britt is getting in
+Allan: Hello Britt
+Carl is getting in
+Allan: Hello Carl
+Britt: Hello Carl
+Dee is getting in
+Allan: Hello Dee
+Britt: Hello Dee
+Carl: Hello Dee
+Carl is getting out
+Allan: Bye Bye Carl
+Britt: Bye Bye Carl
+Dee: Bye Bye Carl
+*/
