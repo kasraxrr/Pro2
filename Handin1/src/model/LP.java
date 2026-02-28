b3a41779ccdac709305d3044c07dc11f3f6f39eb
kasraxrr <kasrafar1383@gmail.com>
2026-02-28 12:06:15 +0100

    yuguyfmkjb

--- Handin1/src/model/LP.java
+++ Handin1/src/model/LP.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class LP {
     private String title;
     private String artist;
@@ -44,11 +46,52 @@ public class LP {
     public LPState getState(){
         return state;
     }
-    public void loan(LP lp,String person){
-        if (getState() instanceof AvailableState || getState() instanceof ReservedState && ){
+    public String getStateString(){
+        return state.getStatusString();
+    }
+    public void loan(String person){
+        if (getState() instanceof AvailableState || getState() instanceof ReservedState && state.getReservedBy().equals(person)){
             this.state=new LoanedState(this,person,flagged);
         }
+    }
 
+    public void reserve(String person){
+        if (getState() instanceof AvailableState && !flagged){
+            this.state=new ReservedState(this,person,flagged);
+        } else if (getState() instanceof LoanedState && !(getState() instanceof LoanedAndReservedState) && !flagged) {
+            this.state=new LoanedAndReservedState(this,state.getLoanedTo(),person,flagged);
+        }
+    }
+
+    public void cancelReservation(){
+        if (getState() instanceof ReservedState){
+            this.state=new AvailableState(this,flagged);
+        }
+        else if (getState() instanceof  LoanedAndReservedState) {
+            this.state=new LoanedState(this,state.getLoanedTo(),flagged);
+        }
+    }
+    public void returnLP(){
+        if (getState() instanceof LoanedState){
+            this.state=new AvailableState(this,flagged);
+        }
+        if (getState() instanceof LoanedAndReservedState && !flagged){
+            this.state=new AvailableState(this,flagged);
+            reserve(state.getReservedBy());
+        }
+        if (getState() instanceof LoanedAndReservedState && flagged){
+            this.state=new AvailableState(this,flagged);
+        }
+    }
+    public String toString(){
+      return   title+artist+year+state+flagged;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        LP lp = (LP) object;
+        return year == lp.year && flagged == lp.flagged && Objects.equals(title, lp.title) && Objects.equals(artist, lp.artist) && Objects.equals(state, lp.state);
     }
 
 }
