c22cb101e5eca1d0593e27a6800fe848e1178c6c
kasraxrr <kasrafar1383@gmail.com>
2026-03-01 09:17:56 +0100

    yuguyfmkjb

--- Handin1/src/viewmodel/MainViewModel.java
+++ Handin1/src/viewmodel/MainViewModel.java
@@ -1,4 +1,211 @@
 package viewmodel;
 
-public class MainViewModel {
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TextInputDialog;
+import model.LP;
+import model.Model;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class MainViewModel implements PropertyChangeListener {
+    private ObservableList<SimpleLPViewModel> list;
+    private ObjectProperty<SimpleLPViewModel> selectedLPProperty;
+    private StringProperty errorProperty;
+    private Model model;
+    private ViewState viewState;
+
+    public MainViewModel(Model model, ViewState viewState)
+    {
+        this.model = model;
+        this.viewState = viewState;
+
+        list = FXCollections.observableArrayList();
+        selectedLPProperty = new SimpleObjectProperty<>();
+
+        errorProperty = new SimpleStringProperty();
+
+        model.addListener(this);
+    }
+
+    public void clear()
+    {
+        list.clear();
+    }
+    public void loadFromModel()
+    {
+        ArrayList<LP> modelList = model.getAllLPs();
+        list.clear();
+        for (int i = 0; i < modelList.size(); i++)
+        {
+            list.add(new SimpleLPViewModel(modelList.get(i)));
+        }
+    }
+
+    public void remove()
+    {
+        try
+        {
+            LP lp = model.getLP(selectedLPProperty.get().getTitleProperty().get(),
+                    selectedLPProperty.get().getArtistProperty().get());
+            if (lp.getStateString().equals("Available"))
+            {
+                model.removeLP(lp);
+            }else
+            {
+                model.flagForRemove(lp);
+            }
+        }
+        catch (Exception e)
+        {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public void loan()
+    {
+        try
+        {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("Loan");
+            dialog.setContentText("Enter name of loaner:");
+            Optional<String> result = dialog.showAndWait();
+            result.ifPresent(s -> model.loan(
+                    model.getLP(selectedLPProperty.get().getTitleProperty().get(),
+                            selectedLPProperty.get().getArtistProperty().get()),s));
+        }
+        catch (Exception e)
+        {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public void reserve()
+    {
+        try
+        {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("Reservation");
+            dialog.setContentText("Enter name of reserver:");
+            Optional<String> result = dialog.showAndWait();
+            result.ifPresent(s -> model.reserve(
+                    model.getLP(selectedLPProperty.get().getTitleProperty().get(),
+                            selectedLPProperty.get().getArtistProperty().get()),s));
+        }
+        catch (Exception e)
+        {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public void returnLP()
+    {
+        try
+        {
+            model.returnLP(model.getLP(selectedLPProperty.get().getTitleProperty().get(),
+                    selectedLPProperty.get().getArtistProperty().get()));
+        }
+        catch (Exception e)
+        {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public void cancel()
+    {
+        try
+        {
+            model.cancel(model.getLP(selectedLPProperty.get().getTitleProperty().get(),
+                    selectedLPProperty.get().getArtistProperty().get()));
+        }
+        catch (Exception e)
+        {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public ObservableList<SimpleLPViewModel> getAll()
+    {
+        return list;
+    }
+
+    public void setSelected(SimpleLPViewModel lpViewModel)
+    {
+        if(lpViewModel != null)
+        {
+            selectedLPProperty.set(lpViewModel);
+            viewState.setTitle(lpViewModel.getTitleProperty().get());
+            viewState.setArtist(lpViewModel.getArtistProperty().get());
+        }else
+        {
+            viewState.setTitle(null);
+            viewState.setArtist(null);
+        }
+    }
+
+    public StringProperty getErrorProperty()
+    {
+        return errorProperty;
+    }
+
+    private void removeSimpleLP(LP lp)
+    {
+        list.remove(new SimpleLPViewModel(lp));
+    }
+
+    private void addSimpleLP(LP lp)
+    {
+        list.add(new SimpleLPViewModel(lp));
+    }
+
+    @Override public void propertyChange(PropertyChangeEvent evt)
+    {
+        Platform.runLater(() -> {
+            switch(evt.getPropertyName())
+            {
+                case "remove":
+                    removeSimpleLP((LP) evt.getNewValue());
+                    break;
+                case "add":
+                    addSimpleLP((LP) evt.getNewValue());
+                    break;
+                case "loan", "reserve", "flag", "return", "cancel":
+                    System.out.println("loading model");
+                    loadFromModel();
+                    break;
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Handin1/src/viewmodel/ViewState.java
+++ Handin1/src/viewmodel/ViewState.java
@@ -4,8 +4,11 @@ import model.LP;
 
 public class ViewState {
     private LP selectedLP;
+    private String title;
+    private String artist;
     public ViewState(){
-
+    this.title=null;
+    this.artist=null;
     }
 
     public void setSelectedLP(LP selectedLP) {
@@ -14,5 +17,21 @@ public class ViewState {
 
     public LP getSelectedLP() {
         return selectedLP;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
     }
 }
