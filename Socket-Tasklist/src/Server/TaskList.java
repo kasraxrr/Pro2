b16d1796089287d015f417aac9f8584a04f7ab6e
kasraxrr <kasrafar1383@gmail.com>
2026-03-18 20:58:06 +0100

    yuguyfmkjb

--- Socket-Tasklist/src/Server/TaskList.java
+++ Socket-Tasklist/src/Server/TaskList.java
@@ -1,4 +1,28 @@
 package Server;
 
+import java.util.ArrayList;
+
 public class TaskList {
+    private ArrayList<Task>tasks;
+
+    public TaskList(){
+        this.tasks=new ArrayList<>();
+    }
+    public void add(Task task){
+        this.tasks.add(task);
+    }
+    public Task getAndRemoveNextTask(int index){
+        tasks.remove(index+1);
+        return tasks.get(index);
+    }
+    public int size(){
+        return tasks.size();
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "tasks=" + tasks +
+                '}';
+    }
 }
