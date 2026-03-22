1d00f112f0490560aa1eafca591da09423a37ab0
kasraxrr <kasrafar1383@gmail.com>
2026-03-22 13:16:04 +0100

    yuguyfmkjb

--- Socket-SimpleUppercase/src/Client.java
+++ Socket-SimpleUppercase/src/Client.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+        final int port=7;
+        final String host="192.168.8.104";
+
+        Scanner scanner=new Scanner(System.in);
+
+        Socket socket=new Socket(host,port);
+
+        BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        PrintWriter out=new PrintWriter(socket.getOutputStream());
+
+        System.out.println("send your first message to server");
+        String req=scanner.nextLine();
+        out.println(req);
+
+        System.out.println("server said : "+in.readLine());
+
+        System.out.println("send your request");
+        String req2=scanner.nextLine();
+        out.println(req2);
+        System.out.println("server said : "+in.readLine());
+        socket.close();
+    }
 }
--- Socket-SimpleUppercase/src/Server.java
+++ Socket-SimpleUppercase/src/Server.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Server {
     final static int port=7;
@@ -12,15 +13,15 @@ public class Server {
         ServerSocket welcome =new ServerSocket(port);
 
         while (true){
-            System.out.println("Wating for requests");
+            System.out.println("Wating for requests...");
             Socket socket=welcome.accept();
+            System.out.println(Arrays.toString(socket.getLocalAddress().getAddress()));
             BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out=new PrintWriter(socket.getOutputStream());
             String req=in.readLine();
             out.println("welcome to my Uppercase converter");
             String question=in.readLine();
             out.println("here is your uppercase : "+question.toUpperCase());
-            socket.close();
         }
 
     }
