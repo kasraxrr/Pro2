9e39c5d05612e83be044a90b4c20812a48b27187
kasraxrr <kasrafar1383@gmail.com>
2026-03-22 13:07:28 +0100

    yuguyfmkjb

--- Socket-SimpleUppercase/src/Server.java
+++ Socket-SimpleUppercase/src/Server.java
@@ -1,4 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    final int port=7;
+    final static int port=7;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket welcome =new ServerSocket(port);
+
+        while (true){
+            System.out.println("Wating for requests");
+            Socket socket=welcome.accept();
+            BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out=new PrintWriter(socket.getOutputStream());
+            String req=in.readLine();
+            out.println("welcome to my Uppercase converter");
+            String question=in.readLine();
+            out.println("here is your uppercase : "+question.toUpperCase());
+            socket.close();
+        }
+
+    }
 
 }
