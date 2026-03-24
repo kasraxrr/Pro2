271328fda1a84ca20807c8c4b5126a5de305a914
kasraxrr <kasrafar1383@gmail.com>
2026-03-24 22:56:22 +0100

    yuguyfmkjb

--- Socket-UDP-Uppercase/src/Client.java
+++ Socket-UDP-Uppercase/src/Client.java
@@ -1,2 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Scanner;
+
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+        final int port=7;
+        final String host="192.168.8.104";
+
+        Scanner scanner=new Scanner(System.in);
+
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        InetAddress IPAddress = InetAddress.getByName(host);
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+
+        System.out.print("Write a line for the server: ");
+        String sentence = scanner.nextLine();
+        System.out.println("Client> " + sentence);
+        sendData = sentence.getBytes();
+// Create datagram with data-to-send, length, IP addr, port
+        DatagramPacket sendPacket = new DatagramPacket(sendData,
+                sendData.length,IPAddress, port);
+// Send datagram to server
+        clientSocket.send(sendPacket);
+// Read datagram from server.
+        DatagramPacket receivePacket = new DatagramPacket(receiveData,
+                receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedStc = new String(receivePacket.getData()).trim();
+        System.out.println("Server> " + modifiedStc);
+// Close connection.
+        clientSocket.close();
+
+    }
 }
--- Socket-UDP-Uppercase/src/Server.java
+++ Socket-UDP-Uppercase/src/Server.java
@@ -1,2 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Arrays;
+
 public class Server {
+    final static int port=7;
+
+    public static void main(String[] args) throws IOException {
+        DatagramSocket serverSocket =new DatagramSocket(port);
+
+        while (true){
+            System.out.println("Wating for requests...");
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData,
+                    receiveData.length);
+
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData()).trim();
+
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            System.out.println("Client> " + sentence);
+            String capitalizedSentence = sentence.toUpperCase();
+            System.out.println("Server> " + capitalizedSentence);
+            byte[] sendData = new byte[1024];
+            sendData = capitalizedSentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData,
+                    sendData.length, IPAddress, port);
+
+            serverSocket.send(sendPacket);
+        }
+
+    }
+
 }
